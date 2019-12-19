6d60ec4b9d9b8c87b8253e93ea29d793c5cb1305
clediano estefenon <cledianoestefenon@gmail.com>
2019-12-19 09:44:01 -0300

    Carregando serviço.

---
+++ backend/src/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+require('dotenv').config({
+    path: path.resolve(__dirname, '..', '.env')
+});
+
+const app = express();
+
+app.use(cors());
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+    socket.on('connectRoom', box => {
+        socket.join(box);
+    })
+});
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+
+    return next();
+})
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/file', express.static(path.resolve(__dirname, '..', 'tmp')));
+
+app.use(require('./routes'));
+
+server.listen(process.env.PORT || 3333);
